3fcdcf3c6d2399c844cf6838d00368047d9717fa
Alvin <spaded06543@yahoo.com.tw>
2015-05-01 17:49:41 +0800

    add main in test.py

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import jieba.posseg as pseg
+import sys
+def getPOSfile(inputfile):
+    f = open(inputfile, "r")
+    q = None
+    q2 = None
+    for line in f:
+        q = line.split("\t")
+        q2 = q2 + pseg.cut(q[2])
 
+    print("Done~\n")
+    return q2
 
-f = open("train.txt", "r")
-out = open("POS.txt", "w")
-q = None
-q2 = None
-for line in f:
-    q = line.split("\t")
-    out.write(q[1] + "/")
-    q2 = pseg.cut(q[2])
-    for w in q2:
-        out.write(w.flag + "/")
-out.write("\n")
-
-print "Done~\n"
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("usage : test.py [inputfile].\n")
+        sys.exit(0)
+    A = getPOSfile(sys.argv[1])
+    for l in A:
+        for w in l:
+            print(w.flag + "\n")
+        print("\n")
