c3e27b397890509e1b224e87e48f27296d6f32dd
Alvin <spaded06543@yahoo.com.tw>
2015-05-01 17:16:09 +0800

    modify tab in test.py

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ out = open("POS.txt", "w")
 q = None
 q2 = None
 for line in f:
-	q = line.split("\t")
-	out.write(q[1] + "/")
-	q2 = pseg.cut(q[2])
-	for w in q2:
-		out.write(w.flag + "/")
-	out.write("\n")
+    q = line.split("\t")
+    out.write(q[1] + "/")
+    q2 = pseg.cut(q[2])
+    for w in q2:
+        out.write(w.flag + "/")
+    out.write("\n")
 
 print "Done~\n"
