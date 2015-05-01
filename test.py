5c03c8e1ed01fc13b86c9690a1f11ec66a29bee5
Alvin <spaded06543@yahoo.com.tw>
2015-05-01 17:10:38 +0800

    upload data

---
+++ test.py
@@ -0,0 +1,16 @@
+import jieba.posseg as pseg
+
+
+f = open("train.txt", "r")
+out = open("POS.txt", "w")
+q = None
+q2 = None
+for line in f:
+	q = line.split("\t")
+	out.write(q[1] + "/")
+	q2 = pseg.cut(q[2])
+	for w in q2:
+		out.write(w.flag + "/")
+	out.write("\n")
+
+print "Done~\n"
