c1161da42bc4e6d48abe5dbb23a64ea51bdb5d24
MROS <yc1043@gmail.com>
2015-05-03 14:34:17 +0800

    可算ngram次數，還沒算機率

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 自然語言作業
+
+## 想法
+分析有冗字出現的ngram pattern
+以及正確的ngram pattern
+將兩者差異最大的視為冗字的pattern
+之後檢測裡面是否出現此pattern
--- test.py
+++ test.py
@@ -1,22 +1,62 @@
 import jieba.posseg as pseg
 import sys
-def getPOSfile(inputfile):
-    f = open(inputfile, "r")
-    q = None
-    q2 = None
-    for line in f:
-        q = line.split("\t")
-        q2 = q2 + pseg.cut(q[2])
 
-    print("Done~\n")
-    return q2
+
+class Sentence:
+    def __init__(self, sentence, redundant):
+        self.sentence = sentence
+        self.sentence_with_pos = list(pseg.cut(sentence))
+        self.redundant = redundant
+        print("redundant: {0}, pos: {1}".format(self.redundant, list(map(lambda a: a.flag, self.sentence_with_pos))))
+
+
+# 做有平滑跟無平滑版本
+class Ngram:
+    def __init__(self, n, sentences):
+        self.n = n
+        self.count(sentences)
+
+    # 回傳目前的計數
+    # 用個dictionary來紀錄
+    # None 代表在字首
+    def count(self, sentences):
+        count = {}
+        for sentence in sentences:
+            s_with_pos = sentence.sentence_with_pos
+            for position in range(0, len(s_with_pos)):
+                prefix = []
+                for shift in range(-self.n, 0):
+                    if position + shift < 0:
+                        prefix.append(None)
+                    else:
+                        prefix.append(s_with_pos[position + shift].flag)
+                prefix = tuple(prefix)
+                th = s_with_pos[position].flag
+                try:
+                    count[(prefix, th)] += 1
+                except KeyError:
+                    count[(prefix, th)] = 1
+        print(count)
+
+
+def get_ngram(n, train_data):
+    f = open(train_data, "r")
+    correct = []
+    incorrect = []
+    for line in f:
+        redundant = (line.split("\t")[1] == '1')
+        sentence = line.split("\t")[2]
+        if redundant:
+            incorrect.append(Sentence(sentence, redundant))
+        else:
+            correct.append(Sentence(sentence, redundant))
+    print("詞性分析結束\n")
+    return (Ngram(n, correct), Ngram(n, incorrect))
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("usage : test.py [inputfile].\n")
+    if len(sys.argv) != 3:
+        print("usage : test.py [input_file] [n].\n")
         sys.exit(0)
-    A = getPOSfile(sys.argv[1])
-    for l in A:
-        for w in l:
-            print(w.flag + "\n")
-        print("\n")
+    n = int(sys.argv[2])
+    a = get_ngram(n, sys.argv[1])
